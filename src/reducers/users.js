295990a383b7f784dbbca74c03c0825a11277318
saurabh30 <gupta.saurabh711@gmail.com>
2019-01-26 20:23:44 +0530

    continuing pvs work

---
+++ src/reducers/users.js
@@ -0,0 +1,19 @@
+import * as types from '../constatnts/ActionTypes'
+
+const messages = (state = [],action) => {
+    switch (action.type) {
+        case types.ADD_USER:
+            return state.concat([
+                {
+                    name: action.name,
+                    id: action.id
+                }
+            ])
+        case types.USERS_LIST:
+            return action.user    
+        default:
+            return state    
+    }
+}
+
+export default users
